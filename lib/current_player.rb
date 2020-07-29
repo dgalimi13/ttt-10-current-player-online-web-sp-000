794b3ef7a59e40aa9a6ed3e484747a1f16e6f96b
danny galimi <dannygalimi13@aim.com>
2020-07-29 20:17:44 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,7 @@
 def turn_count(board)
   counter = 0
-  board.each do |board|
-    if #{board} == "X" || #{board} == "O"
+  board.each do |move|
+    if #{move} == "X" || #{move} == "O"
     counter += 1
   end 
 end
